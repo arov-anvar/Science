87737d7cd0d3c93a8717d04f0860b449573b451e
arov-anvar <anvarsv963@gmail.com>
2021-06-10 01:58:02 +0300

    init

---
+++ app/src/main/java/com/anvar/science/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anvar.science
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.science.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
